be8488748b5ce7315baaef58f8011adeacf87b36
JJ Merelo <jjmerelo@gmail.com>
2022-06-23 19:26:16 +0200

    Añade tests

---
+++ code/node/src/rock_en_samil.js
@@ -0,0 +1,13 @@
+
+export function rock() {
+    return "En Samil";
+}
+
+export function vamosABailar( elQué ) {
+    if ( elQué.toLowerCase() == "el rock" ) {
+	return "En Samil"
+    } else {
+	return "Rock and Roll Toda la Noche Hasta Que Caiga El Sol"
+    }
+    
+}
