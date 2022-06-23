3a83909d12b16571f6c7bc9bb56c7054ebff62ca
JJ Merelo <jjmerelo@gmail.com>
2022-06-23 19:30:23 +0200

    Crea workflow

--- code/node/test/rock.test.js
+++ code/node/test/rock.test.js
@@ -2,6 +2,7 @@ import test from 'node:test';
 import assert from 'assert/strict';
 
 import { rock, vamosABailar } from "../src/rock-en-samil.js";
+
 test("Rock", () => {
     assert.equal( rock(), "En Samil" );
 }
